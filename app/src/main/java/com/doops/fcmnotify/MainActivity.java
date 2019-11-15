fc77cbeb47efbdc77b4e52398558ddcf4a6ffd79
Doops <vickyd178>
2019-11-15 19:41:53 +0530

    added Circular Progressbar

--- app/src/main/java/com/doops/fcmnotify/MainActivity.java
+++ app/src/main/java/com/doops/fcmnotify/MainActivity.java
@@ -2,6 +2,7 @@ package com.doops.fcmnotify;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -18,14 +19,49 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    CircularProgressBar circularProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        circularProgressBar = findViewById(R.id.circularProgressBar);
+
+// Set Progress
+        circularProgressBar.setProgress(65f);
+// or with animation
+        circularProgressBar.setProgressWithAnimation(65f, (long) 1000); // =1s
+
+// Set Progress Max
+        circularProgressBar.setProgressMax(200f);
+
+// Set ProgressBar Color
+        circularProgressBar.setProgressBarColor(Color.BLACK);
+// or with gradient
+        circularProgressBar.setProgressBarColorStart(Color.GRAY);
+        circularProgressBar.setProgressBarColorEnd(Color.RED);
+        circularProgressBar.setProgressBarColorDirection(CircularProgressBar.GradientDirection.TOP_TO_BOTTOM);
+
+// Set background ProgressBar Color
+        circularProgressBar.setBackgroundProgressBarColor(Color.GRAY);
+// or with gradient
+        circularProgressBar.setBackgroundProgressBarColorStart(Color.WHITE);
+        circularProgressBar.setBackgroundProgressBarColorEnd(Color.RED);
+        circularProgressBar.setBackgroundProgressBarColorDirection(CircularProgressBar.GradientDirection.TOP_TO_BOTTOM);
+
+// Set Width
+        circularProgressBar.setProgressBarWidth(3f); // in DP
+        circularProgressBar.setBackgroundProgressBarWidth(2f); // in DP
+
+// Other
+        circularProgressBar.setRoundBorder(true);
+        circularProgressBar.setStartAngle(180f);
+        circularProgressBar.setProgressDirection(CircularProgressBar.ProgressDirection.TO_RIGHT);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
